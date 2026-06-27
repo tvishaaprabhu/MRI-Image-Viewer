8fc2f4415a1785e400ff89ccd57c9636f9c01a93
tvishaaprabhu <66212397+tvishaaprabhu@users.noreply.github.com>
2026-06-27 19:28:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,7 @@ img = st.file_uploader("Upload MRI image", type=["jpg", "jpeg", "png"])
 if img is not None:
     mri = Image.open(img)
     st.image(mri)
+    st.write(f"Size: {image.size[0]} x {image.size[1]} px")
+    st.write(f"Mode: {image.mode}")
+    st.write(f"Format: {image.format}")
  
