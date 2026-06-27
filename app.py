9e235d61a755fba745dda38207eeb933d2a8a30e
tvishaaprabhu <66212397+tvishaaprabhu@users.noreply.github.com>
2026-06-27 19:33:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import streamlit as st
 from PIL import Image
  
-img = st.file_uploader("Upload MRI image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Upload MRI image", type=["jpg", "jpeg", "png"])
  
-if img is not None:
-    mri = Image.open(img)
-    st.image(mri)
-    st.write(f"Size: {image.size[0]} x {image.size[1]} px")
-    st.write(f"Mode: {image.mode}")
-    st.write(f"Format: {image.format}")
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image)
+    with col2:
+        st.table({
+            "Property": ["Width", "Height", "Mode", "Format", "File Size"],
+            "Value": [
+                f"{image.size[0]} px",
+                f"{image.size[1]} px",
+                image.mode,
+                image.format or "N/A",
+                f"{uploaded_file.size / 1024:.1f} KB"
+            ]
+        })
  
